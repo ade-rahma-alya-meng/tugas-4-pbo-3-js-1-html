e2dc88a9ada69f816ff5817479bbe59533dfc670
ade-rahma-alya-meng <alyaalya201911200@gmail.com>
2025-09-15 10:25:53 +0700

    Update awalan.js

--- awalan.js
+++ awalan.js
@@ -1,10 +1,11 @@
 var yakin = confirm("Apakah Kamu Yakin Ingin Membuka Form di page ini ?");
     if (yakin) {
         alert('Selamat Membuat Form Sendiri');
-        var paw = prompt("Tuliskan (contoh: ngantuk, capek, semangat):", "ngantuk");
-        var jalpa = prompt("Tuliskan aktivitasmu sore (contoh: tugas, olahraga, menggambar):", "tidur");
+        var paw = prompt("Kamu suka ngapain? (contoh: belajar, baca buku, tidur):", "TIDUR");
+        var jalpa = prompt("Kamu suka tugas apa? (contoh: coding, olahraga, menggambar):", "CODING");
         // tampilkan teks di console (tidak menghapus form)
         console.log(">>> Kamu berhasil membuka halaman Formulir <<<");
     } else {
          window.location = "https://www.instagram.com/the.jsuniverse?igsh=MTM4bGt0b244bnNtdw==";
+
     }
